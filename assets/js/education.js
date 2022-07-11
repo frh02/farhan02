41c2f65f59ea9c8e2fc621b0f3acc6e1328e183d
frh02 <farhanrahman02@gmail.com>
2022-07-11 11:08:23 -0400

    made changes

--- assets/js/education.js
+++ assets/js/education.js
@@ -5,44 +5,44 @@ AOS.init();
 const moocs = document.querySelector(".moocs");
 const moocscards = [
   {
-    title: "Data Science",
+    title: "Neural Networks and Deep Learning",
     cardImage: "assets/images/education-page/coursera2.svg",
-    moocLink: "https://www.coursera.org/browse/data-science",
+    moocLink: "https://www.coursera.org/account/accomplishments/specialization/certificate/9AYE5FVKMQC6",
   },
   {
-    title: "Cryptography",
-    cardImage: "assets/images/education-page/udemy.svg",
-    moocLink: "https://www.udemy.com/course/cryptography-for-beginners/",
-  },
-  {
-    title: "Machine Learning",
+    title: "TensorFlow Developer",
     cardImage: "assets/images/education-page/coursera2.svg",
-    moocLink: "https://www.coursera.org/learn/machine-learning",
+    moocLink: "https://www.coursera.org/account/accomplishments/specialization/certificate/ENA4ZQLC39PZ",
   },
   {
-    title: "Introduction to HTML 5",
-    cardImage: "assets/images/education-page/udacity.svg",
-    moocLink: "https://www.udacity.com/course/intro-to-html-and-css--ud001",
-  },
-  {
-    title: "Introduction to CSS 3",
-    cardImage: "assets/images/education-page/udacity.svg",
-    moocLink: "https://www.udacity.com/course/intro-to-html-and-css--ud001",
-  },
-  {
-    title: "Javascript",
-    cardImage: "assets/images/education-page/udacity.svg",
-    moocLink: "https://www.udacity.com/course/intro-to-javascript--ud803",
-  },
-  {
-    title: "Bootstrap 4",
+    title: "Build and Deploy Machine Learning App in Cloud with Python",
     cardImage: "assets/images/education-page/udemy.svg",
-    moocLink: "https://www.udemy.com/course/bootstrap-4-tutorials/",
+    moocLink: "https://www.udemy.com/certificate/UC-fdad2edd-8599-4770-84d3-00609a7ac7db/",
   },
   {
-    title: "Intro to React",
+    title: "Reinforcement Learning",
+    cardImage: "assets/images/education-page/coursera2.svg",
+    moocLink: "https://www.coursera.org/account/accomplishments/specialization/certificate/GZPK4K7B2SMW",
+  },
+  {
+    title: "Web Developer Bootcamp with Flask and Python",
+    cardImage: "assets/images/education-page/udemy.svg",
+    moocLink: "https://www.udemy.com/course/web-developer-bootcamp-flask-python/",
+  },
+  {
+    title: "Intro to Deep Learning with PyTorch",
+    cardImage: "assets/images/education-page/udacity.svg",
+    moocLink: "https://www.udacity.com/course/deep-learning-pytorch--ud188",
+  },
+  {
+    title: "NLP - Natural Language Processing with Python",
+    cardImage: "assets/images/education-page/udemy.svg",
+    moocLink: "https://www.udemy.com/course/nlp-natural-language-processing-with-python/",
+  },
+  {
+    title: "CS50's Introduction to Game Development",
     cardImage: "assets/images/education-page/edx.svg",
-    moocLink: "https://www.edx.org/learn/reactjs",
+    moocLink: "https://www.edx.org/course/cs50s-introduction-to-game-development",
   },
   {
     title: "Intro to React Native",
@@ -51,20 +51,20 @@ const moocscards = [
       "https://www.coursera.org/lecture/react-native/introduction-to-react-native-Eax0D",
   },
   {
-    title: "NodeJS, Express and MongoDB",
+    title: "Generative Adversarial Networks (GANs)",
     cardImage: "assets/images/education-page/coursera2.svg",
-    moocLink: "https://www.coursera.org/learn/server-side-nodejs",
+    moocLink: "https://www.coursera.org/account/accomplishments/specialization/certificate/KVZPBCVPLQ6N",
   },
   {
-    title: "XML-AJAX",
+    title: "The Complete SQL Bootcamp 2022: Go from Zero to Hero",
     cardImage: "assets/images/education-page/udemy.svg",
-    moocLink: "https://www.udemy.com/course/xml-from-beginner-to-expert/",
+    moocLink: "https://www.udemy.com/course/the-complete-sql-bootcamp/",
   },
   {
-    title: "Data Structures & Algorithms",
+    title: "Intro to Cloud Computing",
     cardImage: "assets/images/education-page/udacity.svg",
     moocLink:
-      "https://www.udacity.com/course/data-structures-and-algorithms-nanodegree--nd256",
+      "https://www.udacity.com/course/intro-to-cloud-computing--ud080",
   },
 ];
 
--- assets/js/techstack.js
+++ assets/js/techstack.js
@@ -5,6 +5,11 @@ AOS.init();
 const techStackCards = document.querySelector(".techstack-box");
 const techStack = [
   {
+    langImage: "assets/images/techstack-page/python.png",
+    langName: "Python",
+    langDesc: "<li>Python is an interpreted high-level general-purpose programming language</li>",
+  },
+  {
     langImage: "assets/images/techstack-page/html.png",
     langName: "HyperText Markup Language",
     langDesc: "<li>The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser</li>",
@@ -20,14 +25,9 @@ const techStack = [
     langDesc: "<li>JavaScript, often abbreviated JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS</li>",
   },
   {
-    langImage: "assets/images/techstack-page/node.png",
+    langImage: "assets/images/techstack-page/docker.png",
     langName: "Node",
     langDesc: "<li>Node.js is an open-source, cross-platform, back-end JavaScript runtime environment that runs on the V8 engine and executes JavaScript code outside a web browser</li>",
-  },
-  {
-    langImage: "assets/images/techstack-page/python.png",
-    langName: "Python",
-    langDesc: "<li>Python is an interpreted high-level general-purpose programming language</li>",
   },
   {
     langImage: "assets/images/techstack-page/cpp.png",
@@ -40,17 +40,22 @@ const techStack = [
     langDesc: "<li>Git is software for tracking changes in any set of files, usually used for coordinating work among programmers collaboratively developing source code during software development.</li>",
   },
   {
-    langImage: "assets/images/techstack-page/bootstrap.png",
+    langImage: "assets/images/techstack-page/pytorch.png",
     langName: "Bootstrap",
     langDesc: "<li>Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development</li>",
   },
   {
-    langImage: "assets/images/techstack-page/react.png",
+    langImage: "assets/images/techstack-page/tf.png",
     langName: "React",
     langDesc: "<li>React is a free and open-source front-end JavaScript library for building user interfaces based on UI components</li>",
   },
   {
     langImage:"assets/images/techstack-page/c.png",
+    langName:"C",
+    langDesc:"<li>C is a general-purpose, procedural computer programming language supporting structured programming, lexical variable scope, and recursion, with a static type system</li>"
+  },
+  {
+    langImage:"assets/images/techstack-page/keras.png",
     langName:"C",
     langDesc:"<li>C is a general-purpose, procedural computer programming language supporting structured programming, lexical variable scope, and recursion, with a static type system</li>"
   },
